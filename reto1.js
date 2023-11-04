98f1eecd26460a7ef77604d743df2fed1602b8b7
PauDevelop <pablo.arroyo.pons@outlook.es>
2023-11-04 11:45:49 +0100

    Añadido reto semana 1

---
+++ reto1.js
@@ -0,0 +1,11 @@
+function patronMensajes(mensaje) {
+    const patron = {}
+    mensaje.split(' ').forEach(element => {
+        patron[element] = (patron[element] || 0) + 1
+    })
+
+    const result = JSON.stringify(patron).replaceAll('"', '').replaceAll(':', '').replaceAll(',', '').replace('{', '').replace('}', '')
+
+    return result
+}
+
